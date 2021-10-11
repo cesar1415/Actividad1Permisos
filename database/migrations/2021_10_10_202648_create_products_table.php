ab86621ac052476177f4ae738416065c434f1110
Stiven Pineda <jspineda74@misena.edu.co>
2021-10-11 17:55:15 -0500

    Cambio tipo de dato a campo description

--- database/migrations/2021_10_10_202648_create_products_table.php
+++ database/migrations/2021_10_10_202648_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->String('name');
-            $table->String('description');
+            $table->text('description');
             $table->integer('cantidad');
             $table->timestamps();
         });
